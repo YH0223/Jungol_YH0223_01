48502e9cc0b73000835780d60b631b5e5a7b920b
JYH <https://github.com/YH0223/Titanic_JYH>
2024-12-30 17:12:22 +0900

    asdasd

---
+++ src/LC_528/Main.java
@@ -0,0 +1,15 @@
+package LC_528;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		System.out.println(input);
+		if(input.charAt(0)=='-') {
+			System.out.println("minus");
+		}
+		
+	}
+}
